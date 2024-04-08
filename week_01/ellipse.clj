d334b5009464dc0cf6feda1a7e01050670f13884
Julia <julia.kannengiesser@th-bingen.de>
2024-04-08 12:16:53 +0200

    Meine Lösung für Übung 1

--- week_01/ellipse.clj
+++ week_01/ellipse.clj
@@ -1,3 +1,17 @@
 (println "Dieses Programm berechnet die Fläche einer Ellipse")
 
 ;; ADD CODE HERE 
+
+(require '[clojure.math :as math])
+
+(println "Länge der ersten Achse:")
+
+(def x (read-line))
+
+(println "Länge der zweiten Achse:")
+
+(def y (read-line))
+
+(def result (* math/PI (/ (read-string x) 2) (/ (read-string y) 2)))
+
+(println (str "Die Fläche ist: " result))
