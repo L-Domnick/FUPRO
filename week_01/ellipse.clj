465efb39700ad50515f43d2ff436c06b11fcd6e4
Christoph <A2!CZuABcMwrvKAd~SNY?xB8{Tx-}jw_d>
2024-04-08 22:28:44 +0200

    Lösung Übung Woche 01 Christoph Eckart

--- week_01/ellipse.clj
+++ week_01/ellipse.clj
@@ -1,3 +1,14 @@
+(ns ellipse)
+
 (println "Dieses Programm berechnet die Fläche einer Ellipse")
 
-;; ADD CODE HERE 
+(defn ellipse_area [a b] (* Math/PI (/ a 2) (/ b 2)))
+
+(println "Länge der ersten Achse:")
+(def a_axis (read-line))
+
+(println "Länge der zweiten Achse:")
+(def b_axis (read-line ))
+
+(println "Die Fläche ist " (ellipse_area (read-string a_axis) (read-string b_axis)))
+
