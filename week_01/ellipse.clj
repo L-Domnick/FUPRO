51cddf22677da2a4849e5a94304362a470339b9e
Jenny Feist <jenny.feist@th-bingen.de>
2024-04-09 16:30:37 +0200

    loesung_week01

--- week_01/ellipse.clj
+++ week_01/ellipse.clj
@@ -1,3 +1,11 @@
 (println "Dieses Programm berechnet die Fläche einer Ellipse")
 
-;; ADD CODE HERE 
+(println "Länge der ersten Achse:")
+(def a (read-line))
+
+(println "Länge der zweiten Achse:")
+(def b (read-line))
+
+(def result (* Math/PI (* (/ (read-string a) 2) (/ (read-string b) 2))))
+
+(println (str "Die Fläche ist: " result))
