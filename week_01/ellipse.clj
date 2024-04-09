0f549436451756c64196e3d756b148e4b465d4af
L-Domnick <166517337+L-Domnick@users.noreply.github.com>
2024-04-09 18:15:42 +0200

    .

--- week_01/ellipse.clj
+++ week_01/ellipse.clj
@@ -1,3 +1,12 @@
-(println "Dieses Programm berechnet die Fläche einer Ellipse")
+(println "Dieses Programm berechnet die Flaeche einer Ellipse")
 
 ;; ADD CODE HERE 
+
+(println "Laenge der ersten Achse:")
+(def axis_a (read-line))
+(println "Laenge der zweiten Achse:")
+(def axis_b (read-line))
+
+(def res (* Math/PI (/ (read-string axis_a) 2) (/ (read-string axis_b) 2)))
+
+(println "Die Flaeche ist: " res)
